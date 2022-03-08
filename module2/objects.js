00a7a6dc4da9e7eb19462b460f48704e6921eebd
chetan749-data <chetan.beec.15@acharya.ac.in>
2022-03-08 23:24:25 +0530

    Objects full

--- module2/objects.js
+++ module2/objects.js
@@ -11,5 +11,18 @@ let obj={
         console.log("hello world")
     }
 };
-console.log(obj.friends[2]);
-console.log(obj.address.state)
+// console.log(obj.friends[2]); prints onday
+// console.log(obj.address.state) prints karnataka
+// console.log(obj.sayHi)  //prints         sayHi: [Function: sayHi]
+(obj.sayHi()); // to print or call a functioon   prints hello world
+
+// to add any extra key
+obj.native="Bijapur";
+// console.log(obj)  //native is added
+delete obj.native;
+console.log(obj)  //native is removed
+
+
+// access 2 way
+console.log(obj.Name)
+console.log(obj["Name"])
